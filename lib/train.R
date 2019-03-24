9ebddbb8e27824cb30ec522ec70ee0198129c0e5
Xinyi Hu <xh2383@columbia.edu>
2019-03-24 09:52:32 -0400

    update

--- lib/train.R
+++ lib/train.R
@@ -6,7 +6,7 @@
 ### Project 3
 
 
-train <- function(dat_train, label_train, par=NULL){
+train = function(dat_train, label_train, par=NULL){
   
   ### Train a Gradient Boosting Model (GBM) using processed features from training images
   
@@ -16,35 +16,43 @@ train <- function(dat_train, label_train, par=NULL){
   ### Output: a list for trained models
   
   ### load libraries
-  library("gbm")
-  
+  library(gbm)
+  library(xgboost)
+  library(BayesTree)
+  library(iRF)
+  library(rpart)
+
   ### creat model list
-  modelList <- list()
+  modelList = list()
   
-  ### Train with gradient boosting model
+  ### Train with XGboost
   if(is.null(par)){
-    depth <- 3
+    nr = 100
   } else {
-    depth <- par$depth
+    nr = par$nr
   }
   
   ### the dimension of response arrat is * x 4 x 3, which requires 12 classifiers
   ### this part can be parallelized
   for (i in 1:12){
     ## calculate column and channel
-    c1 <- (i-1) %% 4 + 1
-    c2 <- (i-c1) %/% 4 + 1
-    featMat <- dat_train[, , c2]
-    labMat <- label_train[, c1, c2]
-    fit_gbm <- gbm.fit(x=featMat, y=labMat,
-                       n.trees=200,
-                       distribution="gaussian",
-                       interaction.depth=depth, 
-                       bag.fraction = 0.5,
-                       verbose=FALSE)
-    best_iter <- gbm.perf(fit_gbm, method="OOB", plot.it = FALSE)
-    modelList[[i]] <- list(fit=fit_gbm, iter=best_iter)
+    c1 = (i-1) %% 4 + 1
+    c2 = (i-c1) %/% 4 + 1
+    featMat = dat_train[, , c2]
+    labMat = label_train[, c1, c2]
+    
+    ## fit the model
+    cat("Classifer =", i, "\n")
+    train.xgb = xgb.DMatrix(featMat, label =labMat)
+    #xgb_model = xgboost(data = train.xgb, booster = "gblinear", seed = 1, nrounds = nr, verbose = FALSE, 
+     #                      objective = "reg:linear", eval_metric = "rmse", lambda = 1, 
+      #                     alpha = 0)
+    xgb_model = xgboost(data = train.xgb,booster = "gblinear", seed = 1, nrounds = nr,  
+                        objective = "reg:linear", eval_metric = "rmse", lambda = 1, 
+                        alpha = 0,nfold =3,max_depth = 6, min_child_weight =6,gamma= 0.2, print_every_n = FALSE)
+    
+    modelList[[i]] = list(fit=xgb_model, iter=par)
   }
-  
   return(modelList)
 }
+
