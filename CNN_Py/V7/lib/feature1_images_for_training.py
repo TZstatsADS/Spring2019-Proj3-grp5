b247704487d8d3d416334cafabd325c00d68da5f
elena-du <ed2801@columbia.edu>
2019-03-26 12:46:19 -0400

    V7

---
+++ CNN_Py/V7/lib/feature1_images_for_training.py
@@ -0,0 +1,46 @@
+
+import cv2
+import os 
+import glob
+import matplotlib.pyplot as plt
+
+
+def get_paths(folder_name_string):
+    im_dir = os.path.join(os.getcwd(), folder_name_string)
+    paths = glob.glob(os.path.join(im_dir, "*.jpg"))
+    paths = list(paths)
+    return paths
+
+
+def cropped_imgs(hr_paths):
+    
+    image_size = 32
+    color_dim = 3
+    step = 21
+    padding = 6
+
+    HR_sequence = []
+    
+    for i in range(len(hr_paths)):
+
+            HR=cv2.imread(hr_paths[i])
+            
+            if len(HR.shape) == 3: 
+                h, w, c = HR.shape
+            else:
+                h, w = HR.shape 
+     
+            nx, ny = 0, 0
+            for x in range(0, h - image_size + 1, step):
+                nx += 1; ny = 0
+                for y in range(0, w - image_size + 1, step):
+                    ny += 1
+    
+                    HR_cropped = HR[x + padding: x + padding + image_size, y + padding: y + padding + image_size] 
+                    HR_cropped =  HR_cropped / 255.0
+                    HR_sequence.append(HR_cropped)
+
+    return HR_sequence
+
+
+
---
+++ CNN_Py/V7/lib/feature2_train_test_set.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 21:19:39 2019
+
+@author: elena
+"""
+import numpy as np
+import cv2
+from numpy import array 
+# Train_test data 
+
+def hr_images(images):
+    images_hr = images
+    return images_hr
+
+def lr_images(images_to_downscale, n_scale):
+    
+    images = []
+    for i in  range(len(images_to_downscale)):
+        images.append(cv2.resize(images_to_downscale[i], (images_to_downscale[i].shape[0]//n_scale, images_to_downscale[i].shape[1]//n_scale), interpolation = cv2.INTER_LINEAR))
+    images_lr = images
+    return images_lr
+
+
+def train_test_data(imgs, number_of_images = 2200, train_test_ratio = 0.9):
+
+    number_of_train_images = int(number_of_images * train_test_ratio)
+    
+    x_train = imgs[:number_of_train_images]
+    x_test = imgs[number_of_train_images:number_of_images]
+    
+    x_train_hr = hr_images(x_train)
+    
+    x_test_hr = hr_images(x_test)
+    x_test_lr = lr_images(x_test, 2)
+    
+    x_train_lr = lr_images(x_train, 2)
+    
+    return x_train_lr, x_train_hr, x_test_lr, x_test_hr
+
+def ndarray_to_4dim(ndarray, image_size = 16):
+    four_dim = np.empty([len(ndarray), image_size, image_size, 3])
+    for i in range(len(ndarray)):
+        if ndarray[i].shape == (image_size,image_size,3):
+            four_dim[i] = ndarray[i]
+        
+    return four_dim
+
+    #dtype = np.float32
